6eed1d5baac71b3c991ac5f1ac17dc3cae085407
nikgora <nik.gora.2004@gmail.com>
2024-09-17 10:36:54 +0200

    mb done task

---
+++ HW1/ReadMe.md
@@ -0,0 +1,34 @@
+4\
+Результат роботи \
+Складність роботи Стандартного алгоритма О(n).\
+Судячи з результатів заміру швидкості роботи складність роботи вбудованого методу пошуку максимуму О(n/2). Бо завжди +- у 2 рази він швидше ніж прохід по всім елемеентам масива \
+***Приклади***\
+Enter the number of elements in the array: 100000000\
+Enter the number of processes for MapReduce (maximum 12): 10\
+Max value (max(arr)): 8412931090127, Time: 1.0965943336486816 seconds\
+Max value (standard): 8412931090127, Time: 2.02459979057312 seconds\
+Time for splitting into chunks: 0.4633002281188965 seconds\
+Time for processing chunks: 9.026177167892456 seconds\
+Max value (MapReduce): 8412931090127, Time: 9.910645484924316 seconds, Time without splitting: 9.44734525680542 seconds\
+\
+\
+Enter the number of elements in the array: 10000000\
+Enter the number of processes for MapReduce (maximum 12): 10\
+Max value (max(arr)): 8412931130510, Time: 0.10651707649230957 seconds\
+Max value (standard): 8412931130510, Time: 0.20202255249023438 seconds\
+Time for splitting into chunks: 0.043003082275390625 seconds\
+Time for processing chunks: 0.9677262306213379 seconds\
+Max value (MapReduce): 8412931130510, Time: 1.050729751586914 seconds, Time without splitting: 1.0077266693115234 seconds\
+\
+\
+Enter the number of elements in the array: 1000000\
+Enter the number of processes for MapReduce (maximum 12): 10\
+Max value (max(arr)): 8412908307856, Time: 0.010995149612426758 seconds\
+Max value (standard): 8412908307856, Time: 0.020000457763671875 seconds\
+Time for splitting into chunks: 0.003998517990112305 seconds\
+Time for processing chunks: 0.2682180404663086 seconds\
+Max value (MapReduce): 8412908307856, Time: 0.27721667289733887 seconds, Time without splitting: 0.27321815490722656 seconds\
+\
+Швидкість розбиття маисву на чанки десь 1/4 часу від лінійного однопоточного знаходження максимума, цей час не залежить від кількості процесів, бо він однопоточний.\
+На моєму комп'ютері реалізація MapReduce відбуваєтьмя повільніше, бо доволі багато процесів відбуваютсья в фоні, і коли створюються нові процеси вони не пріотеризовані виконуватися одразу, що критично впливає на швидкість виконання.
+
--- HW1/main.py
+++ HW1/main.py
@@ -1,14 +1,14 @@
 import random
 import time
-from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Pool, cpu_count
 
 
-# Функція для знаходження максимума в частині масиву
+# Function for finding the maximum in a part of an array
 def find_max_chunk(chunk):
     return max(chunk)
 
 
-# Стандартний метод для пошуку максимума
+# Standard method for finding the maximum
 def find_max_standard(arr):
     if len(arr) == 0:
         return None
@@ -19,85 +19,69 @@ def find_max_standard(arr):
     return max_val
 
 
-# Функція для багатопотокового розбиття масиву на чанки
-def split_array_parallel(arr, num_chunks, thread_pool):
+# Function for splitting an array into chunks
+def split_array(arr, num_chunks):
     chunk_size = len(arr) // num_chunks
+    chunks = [arr[i * chunk_size:(i + 1) * chunk_size] for i in range(num_chunks)]
 
-    # Функція для вилучення одного чанка
-    def get_chunk(i):
-        return arr[i * chunk_size:(i + 1) * chunk_size]
-
-    # Паралельне розбиття масиву
-    chunk_indices = list(range(num_chunks))
-    with thread_pool as executor:
-        chunks = list(executor.map(get_chunk, chunk_indices))
-
-    # Додаємо елементи, що залишилися, в останній чанк, якщо є
+    # Add the remaining elements to the last chunk, if any
     remaining_elements = len(arr) % num_chunks
     if remaining_elements > 0:
         chunks[-1].extend(arr[-remaining_elements:])
 
     return chunks
 
-# Реалізація через MapReduce
-def find_max_mapreduce(arr, num_threads, thread_pool):
+
+# Implementation via MapReduce using multiprocessing
+def find_max_mapreduce(arr, num_processes):
     start_time = time.time()
 
-    # Параллельное разбиение на чанки
-    chunks = split_array_parallel(arr, num_threads, thread_pool)
+    # Split the array into chunks
+    chunks = split_array(arr, num_processes)
 
     end_time = time.time()
     print(f"Time for splitting into chunks: {end_time - start_time} seconds")
     split_time = end_time - start_time
-    # Параллельне обчислення максимумів для кожної частини
-    start_time = time.time()
-    with ThreadPoolExecutor(num_threads) as executor:
-        end_time = time.time()
-        print(f"Time for thread creation: {end_time - start_time} seconds")
-        max_values = list(executor.map(find_max_chunk, chunks))
 
-    # Знаходимо загальний максимум
+    # Parallel calculation of maxima for each chunk using multiprocessing
+    start_time = time.time()
+    with Pool(processes=num_processes) as pool:
+        max_values = pool.map(find_max_chunk, chunks)
+
+    end_time = time.time()
+    print(f"Time for processing chunks: {end_time - start_time} seconds")
+
+    # Find the total maximum
     return max(max_values), split_time
 
 
-# Генерація випадкового масиву
+# Generate a random array
 def generate_random_array(size):
     return [random.randint(-1923919391293, 8412931133123) for _ in range(size)]
 
+if __name__ == '__main__':
+    # Input the number of elements from the user
+    size = int(input("Enter the number of elements in the array: "))
+    num_processes = int(input(f"Enter the number of processes for MapReduce (maximum {cpu_count()}): "))
 
-# Введення кількості елементів від користувача
-size = int(input("Введіть кількість елементів у масиві: "))
-num_threads = int(input("Введіть кількість потоків для MapReduce: "))
-thread_pool = ThreadPoolExecutor(num_threads)
+    # Generate random array
+    arr = generate_random_array(size)
 
-# Генеруємо випадковий масив
-arr = generate_random_array(size)
+    # Built-in implementation
+    start_time = time.time()
+    max_value_builtin = max(arr)
+    end_time = time.time()
+    print(f"Max value (max(arr)): {max_value_builtin}, Time: {end_time - start_time} seconds")
 
-# Вбудована реалізація
-start_time = time.time()
-max_value_standard = max(arr)
-end_time = time.time()
-print(f"Max value (max(arr)): {max_value_standard}, Time: {end_time - start_time} seconds")
+    # Standard implementation
+    start_time = time.time()
+    max_value_standard = find_max_standard(arr)
+    end_time = time.time()
+    print(f"Max value (standard): {max_value_standard}, Time: {end_time - start_time} seconds")
 
-# Стандартна реалізація
-start_time = time.time()
-max_value_standard = find_max_standard(arr)
-end_time = time.time()
-print(f"Max value (standard): {max_value_standard}, Time: {end_time - start_time} seconds")
-
-# MapReduce реалізація
-start_time = time.time()
-max_value_mapreduce, split_time = find_max_mapreduce(arr, num_threads, thread_pool)
-end_time = time.time()
-print(f"Max value (MapReduce): {max_value_mapreduce}, Time: {end_time - start_time} seconds, Time with out splitting in chunks: {end_time - start_time - split_time} seconds")
-# Введіть кількість елементів у масиві: 100000
-# Введіть кількість потоків для MapReduce: 100
-# Max value (max(arr)): 8412893583015, Time: 0.0009996891021728516 seconds
-# Max value (standard): 8412893583015, Time: 0.0019989013671875 seconds
-# Time for splitting into chunks: 0.011616706848144531 seconds
-# Time for thread creation: 0.0 seconds
-# Max value (MapReduce): 8412893583015, Time: 0.018630504608154297 seconds, Time with out splitting in chunks: 0.007013797760009766 seconds
-#
-# Process finished with exit code 0
-
-
+    # MapReduce implementation with multiprocessing
+    start_time = time.time()
+    max_value_mapreduce, split_time = find_max_mapreduce(arr, num_processes)
+    end_time = time.time()
+    print(
+        f"Max value (MapReduce): {max_value_mapreduce}, Time: {end_time - start_time} seconds, Time without splitting: {end_time - start_time - split_time} seconds")
